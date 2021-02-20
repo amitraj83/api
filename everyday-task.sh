847fb7f2797292b35b297f738693b4778743f1c0
Amit Rajq <amitraj.2383@gmail.com>
2021-02-20 13:52:48 +0000

    everyday task file created

---
+++ everyday-task.sh
@@ -0,0 +1,3 @@
+/usr/bin/python3 /root/car-compare/api/src/AutoGenerateComparisons.py
+/usr/bin/python3 /root/car-compare/api/src/HTMLGenerator.py
+/usr/bin/python3 /root/car-compare/api/src/SiteMapGenerator.py
