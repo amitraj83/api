e5b038b246248b3872187bdeb3ae184830c91742
Amit Rajq <amitraj.2383@gmail.com>
2021-03-13 14:02:05 +0000

    tweets with hashtags

--- src/twitter/MoreTryWithKeys.py
+++ src/twitter/MoreTryWithKeys.py
@@ -14,16 +14,7 @@ auth=tweepy.OAuthHandler(consumer_key,consumer_secret_key)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
 
-# list = api.home_timeline(1)
-# print(list)
+# for tweet in api.home_timeline(5):
+#     print (tweet.text, tweet.id)
 
-# tweet=input('enter the tweet')
-#Generate text tweet
-api.update_status("Why buy this car? Checkout this https://suggestrank.com/compare/cars/2907/Toyota-Auris-2007-vs-Toyota-Highlander-2007-vs-Toyota-RunX-2007")
-
-# tweet_text=input('enter the tweet ')
-# image_path =input('enter the path of the image ')
-
-#Generate text tweet with media (image)
-# status = api.update_with_media(image_path, tweet_text)
-# api.update_status(tweet_media)
+api.update_status("Think before selecting a Volkswagen? Checkout here \r\n #Volkswagen #compare #cars #comparecars. https://suggestrank.com/compare/cars/3271/Volkswagen-Jetta-2011-vs-Volkswagen-Caddy-2011-vs-Volkswagen-Jetta-2011")
